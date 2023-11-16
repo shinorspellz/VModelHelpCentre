5831f59941942d09250d19563f45e9b8e6cef520
Divine Quansah <divquan@gmail.com>
2023-11-16 06:55:53 +0000

    Add related topics section to page.jsx

--- app/[slug]/[subslug]/page.jsx
+++ app/[slug]/[subslug]/page.jsx
@@ -2,11 +2,12 @@
 import { egHTML, subTopicContent, subTopics } from "@/data";
 import React from "react";
 import "./styles.css";
+import Link from "next/link";
 
 const page = ({ params }) => {
   let contentHTML;
+  const { slug, subslug } = params;
   try {
-    const { slug, subslug } = params;
     contentHTML = subTopicContent[slug][subslug];
   } catch (e) {
     contentHTML = `<article>
@@ -20,8 +21,30 @@ const page = ({ params }) => {
 
   // console.log(contentHTML);
   return (
-    <div className="faq">
-      <div dangerouslySetInnerHTML={{ __html: contentHTML || egHTML }} />
+    <div>
+      <div className="faq">
+        <div dangerouslySetInnerHTML={{ __html: contentHTML || egHTML }} />
+      </div>
+      <div className="mt-7">
+        <h3 className="text-lg mb-3 text-[#edceab]">Related Topics</h3>
+        <div className="flex flex-wrap">
+          {subTopics[slug].map((topic, index) => {
+            return (
+              <Link
+                href={"/" + slug + "/" + topic.link}
+                className="w-1/2 md:w-1/3 lg:w-1/3"
+                key={index}
+              >
+                <div
+                  className={`h-24 border mr-6 mb-6 border-[#a1a1a165] gap-5 flex flex-col justify-center items-center text-center p-2 rounded-md cursor-pointer hover:bg-[#a1a1a165]`}
+                >
+                  <span>{topic.name}</span>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
